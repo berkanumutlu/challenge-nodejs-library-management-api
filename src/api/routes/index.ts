0bb4fca42e4bcf6d3f2068940b303824ad78b995
Berkan Ümütlü <berkanumutlu1@gmail.com>
2024-11-28 23:41:06 +0300

    feat: Added route not found definition

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -6,4 +6,13 @@ const router = Router();
 router.use('/users', userRoutes);
 // ... Route definitions for other endpoints
 
+// Route not found definition
+router.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: `The route ${req.method} ${req.originalUrl} does not exist.`,
+        error: 'Page Not Found'
+    });
+});
+
 export default router;
